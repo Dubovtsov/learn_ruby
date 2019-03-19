ecee653edf2b170afe0ccef4a154ceb5a41b57d2
Dubovtsov <dubwin@yandex.ru>
2019-03-20 00:15:46 +0300

    Update discriminant.rb

--- discriminant.rb
+++ discriminant.rb
@@ -12,11 +12,11 @@ puts "Введите значение c :"
 c = gets.chomp.to_f
 
 d = b**2 - 4 * a * c
-sqrt_d = Math.sqrt(d)
 
 if d < 0
   puts "Дискриминант равен #{d}. Корней нет!"
 elsif d > 0
+  sqrt_d = Math.sqrt(d)
   x1 = (-b + sqrt_d)/2 * a
   x2 = (-b - sqrt_d)/2 * a
   puts "Дискриминант равен #{d}. Корень X1 = #{x1.round(2)}, Корень X2 = #{x2.round(2)}"
