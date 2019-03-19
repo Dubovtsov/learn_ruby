736c4c5436eac47be7bd78d468e880130e4d2af8
Dubwin <dubwin@yandex.ru>
2019-03-19 23:41:03 +0300

    sides array

--- hypotenuse.rb
+++ hypotenuse.rb
@@ -2,35 +2,25 @@
 # Программа определяет является ли треугольник прямоугольным #
 ##############################################################
 
+sides = []
+
 puts "Введите сторону а :"
-a = gets.chomp.to_f
+sides << gets.chomp.to_f
 
 puts "Введите сторону b :"
-b = gets.chomp.to_f
+sides << gets.chomp.to_f
 
 puts "Введите сторону c :"
-c = gets.chomp.to_f
+sides << gets.chomp.to_f
 
-if a >= b && a >= c
-  hypotenuse = a
-  side_a = b
-  side_b = c
-elsif b >= a && b >= c
-  hypotenuse = b
-  side_a = a
-  side_b = c
-elsif c >= a && c >= b
-  hypotenuse = c
-  side_a = a
-  side_b = b						
-end
+a, b, c, = sides.sort!
 
 puts "Треугольник является равнобедренным." if a == b && a != c || a == c && a != b || b == c && b != a
 
 puts "Треугольник является равносторонним." if a == b && b == c && a == c
 
 # проверка по теореме Пифагора
-if (hypotenuse**2) == side_a**2 + side_b**2
+if (c**2) == a**2 + b**2
   puts "Треугольник прямоугольный"
 else
   puts "Треугольник не прямоугольный!"
