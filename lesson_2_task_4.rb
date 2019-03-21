1cfac6ecbb48f385ea0fd395848880cb49904393
Dubovtsov <dubwin@yandex.ru>
2019-03-21 16:15:22 +0300

    abc

--- lesson_2_task_4.rb
+++ lesson_2_task_4.rb
@@ -1,2 +1,15 @@
-string_AZ = Hash[("a".."z").to_a.zip((1..26).to_a)]
-symbol_AZ = Hash[(:a..:z).to_a.zip((1..26).to_a)]
+consonants = ["б", "в", "г", "д", 
+              "ж", "з", "й", "к", 
+              "л", "м", "н", "п", 
+              "р", "с", "т", "ф", 
+              "х", "ц", "ч", "ш",
+               "щ", "ь", "ъ"]
+
+abc = Hash[("а".."я").to_a.zip((1..32).to_a)]
+
+consonants.each do |letter|
+  if abc.include?(letter)
+    abc.delete(letter)
+  end
+end
+puts abc
