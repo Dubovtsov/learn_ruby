63045c354530cf95ef7a1bc5ec056a2053b56b8d
Dubwin <dubwin@yandex.ru>
2019-03-24 20:13:06 +0300

    cart

--- lesson_2/lesson_2_task_6.rb
+++ lesson_2/lesson_2_task_6.rb
@@ -7,23 +7,25 @@ total = 0
 
 loop do
   quantity_and_price = {}
-  puts "Введите название товара:"
-  item = gets.chomp
-  break if item == "стоп"
+  puts "Введите название товара или 'стоп' для завершения ввода:"
+  name_item = gets.chomp
+  break if name_item == "стоп"
 
   puts "Введите цену товара за единицу:"
   price = gets.to_f
-  quantity_and_price["price"] = price
+  quantity_and_price[:price] = price
 
   puts "Введите количество товара:"
   quantity = gets.to_i
-  quantity_and_price["quantity"] = quantity
+  quantity_and_price[:quantity] = quantity
  
-  current_total = price * quantity
-  
-  total += current_total
-  quantity_and_price["current_total"] = current_total
-  user_cart[item] = quantity_and_price
+  user_cart[name_item] = quantity_and_price
 end
-puts "#{user_cart}"
-puts "В корзине товаров на сумму: #{total}"
+
+user_cart.each do |name_item, item|
+  item_total = item[:price] * item[:quantity]
+  puts "#{name_item} - #{item[:quantity]} шт. на сумму #{item_total}"
+  total += item_total
+end
+
+puts "Итого: #{total}"
