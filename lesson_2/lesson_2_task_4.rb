d9cdc2c9fc78c0dce32abae077cee466f1bf864d
Dubovtsov <dubwin@yandex.ru>
2019-03-25 16:40:42 +0300

    ruby v up 2.5.1

--- lesson_2/lesson_2_task_4.rb
+++ lesson_2/lesson_2_task_4.rb
@@ -6,6 +6,6 @@ vowels = ["а", "о", "э", "ы", "у", "я", "е", "ю", "и"]
 
 abc = {}
 ("а".."я").to_a.each_with_index do |value, index|
-  vowels.each { |letter| abc[letter] = index if value == letter }
+  vowels.each { |letter| abc[letter] = index + 1  if value == letter }
 end
 puts abc
--- lesson_2/lesson_2_task_5.rb
+++ lesson_2/lesson_2_task_5.rb
@@ -9,21 +9,13 @@ def month_days(month, year, day)
                 4 => 30, 5 => 31, 6 => 30, 
                 7 => 31, 8 => 31, 9 => 30, 
                 10 => 31, 11 => 30, 12 => 31 }
-      
+    
   # проверка является ли год високосным
   hash_mdays[2] = 29 if year % 400 == 0 || year % 4 == 0
-  # Создаем массив из значение хэша
-  mdays = hash_mdays.values
   # Кол-во дней в году
-  number_of_days_per_year = mdays.inject(0){ |res, mday| res + mday }
-  number_of_the_day_in_the_month = hash_mdays[month] - day
-
-  d = 0
-  hash_mdays.each do |key, value| 
-    break if key > month
-      d += value     
-  end
-  puts d - number_of_the_day_in_the_month
+  number_of_days_per_year = hash_mdays.values.take(month - 1).sum + day
+  # number_of_the_day_in_the_month = hash_mdays[month] - day
+  puts number_of_days_per_year
 end
 
 loop do
@@ -31,25 +23,22 @@ loop do
   date = gets.chomp.split(".")
   date = date.map{ |e| e.to_i}
   
-  u_day = date[0]
-  u_month = date[1]
-  u_year = date[2]
+  user_day = date[0]
+  user_month = date[1]
+  user_year = date[2]
 
   # Минимальная проверка ввода
-  if u_day < 1 || u_day > 31
+  if user_day < 1 || user_day > 31
     puts "Неправильное число"
-  elsif u_month < 1 || u_month > 12
+  elsif user_month < 1 || user_month > 12
     puts "Неправильный месяц"
   # Просто захотел такой интервал
-  elsif u_year < 1000 || u_year > 2500
+  elsif user_year < 1000 || user_year > 2500
     puts "Год должен быть в интервале 1000..2500"
   else
-    month_days(u_month, u_year, u_day)
-    
+    month_days(user_month, user_year, user_day)
+    now = Time.now
+    puts now.yday
     break
   end 
 end
-
-# Проверка кода встроенным методом
-# now = Time.now
-# puts now.yday
