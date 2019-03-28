2107b780285f241122541ef4a7e0fbc96a8206a1
Dubovtsov <dubwin@yandex.ru>
2019-03-28 15:16:23 +0300

    route

--- lesson_3/railway.rb
+++ lesson_3/railway.rb
@@ -42,6 +42,7 @@ class Train
     @type = type
     @number_of_cars = number_of_cars
     @speed = 0
+    @current_station = 0
   end
 
   # Метод набора скорости
@@ -56,12 +57,25 @@ class Train
 
   # Зацепить вагон
   def hook_car
+    # Сначало поезд должен остановиться
     @number_of_cars += 1 if @speed == 0
   end
 
   # Отцепить вагон
   def unhook_car
+    # Сначало поезд должен остановиться
     @number_of_cars -= 1 if @speed == 0 && @number_of_cars > 0
+  end
+
+  def route(route)
+    @route = route
+    @current_station = @route.train_route.first
+    @current_end_station = @route.train_route.last
+
+  end
+
+  def current_station
+    puts @current_station
   end
 
   def next_station
@@ -70,6 +84,14 @@ class Train
 
   def previous_station
 
+  end
+
+  def move_forward
+    
+  end
+
+  def move_backward
+    
   end
 end
 
