7e02c12c7a0265c43d45d19c8ae8fcdb478ec7b8
Dubwin <dubwin@yandex.ru>
2019-04-03 00:36:49 +0300

    include?

--- lesson_4/main.rb
+++ lesson_4/main.rb
@@ -12,13 +12,17 @@ require_relative 'seeds.rb'
 @stations = []
 @routes = []
 
-puts "------------------------------------------------------"
+puts "-----------------------------------------------------------"
 puts "Что вы хотите сделать?"
-puts "Чтобы добавить новую ж/д станцию введите - create station"
-puts "Чтобы добавить новый поезд введите - create train"
-puts "Чтобы добавить новый маршрут введите - create route"
-puts "Завершить выполнение программы - stop"
-puts "------------------------------------------------------"
+puts "-----------------------------------------------------------"
+puts "Чтобы добавить новую ж/д станцию введите -   create station"
+puts "Чтобы добавить новый поезд введите -         create train"
+puts "Чтобы добавить новый маршрут введите -       create route"
+puts "Чтобы добавить остановку в маршрут введите - add stop"
+puts "Список станций -                             stations"
+puts "Список поездов -                             trains"
+puts "Завершить выполнение программы -             stop"
+puts "-----------------------------------------------------------"
 
 loop do
 
@@ -43,45 +47,34 @@ loop do
       type = gets.chomp
       if type == "грузовой"
         @trains << CargoTrain.new(train_number)
-        puts "Добавлен новый поезд: #{train_number}"
+        puts "Добавлен новый грузовой поезд: #{train_number}"
       else
         @trains << PassengerTrain.new(train_number)
-        puts "Добавлен новый поезд: #{train_number}"
+        puts "Добавлен новый пассажирский поезд: #{train_number}"
       end
-      
     when "create route"
       puts "Введите начальную станцию:"
       start_station = gets.chomp
-      # station1 = Station.new(start_station)
+      @stations << Station.new(start_station) unless @stations.include? start_station
+
       puts "Введите конечную станцию:"
       end_station = gets.chomp
-      # station2 = Station.new(end_station)
+      @stations << Station.new(end_station) unless @stations.include? end_station
       @routes << Route.new(start_station, end_station)
-
       puts "Добавлен маршрут от #{start_station} до #{end_station}"
+
+    # when "add stop"
+    #   puts "Введите название остановки:"
+    #   stop = gets.chomp
+    #   @stops << stop unless @stops.include? stop
+        
+    #   add_station()
     else
       puts "Error!"
   end
 end
-# train = Train.new
 
-# route = Route.new
-
-# moscow = Station.new
-
-
-# irb -r ./main.rb
-# метод seed для загрузки данных
-
-
-# Разделить поезда на два типа PassengerTrain и CargoTrain, 
-# сделать родителя для классов, который будет содержать общие методы и свойства
 # Определить, какие методы могут быть помещены в private/protected 
 # и вынести их в такую секцию. В комментарии к методу обосновать, 
 # почему он был вынесен в private/protected
-# Вагоны теперь делятся на грузовые и пассажирские (отдельные классы). 
-# К пассажирскому поезду можно прицепить только пассажирские, к грузовому - грузовые. 
-# При добавлении вагона к поезду, объект вагона должен передаваться 
-# как аругмент метода и сохраняться во внутреннем массиве поезда, 
-# в отличие от предыдущего задания, где мы считали только кол-во вагонов. 
-# Параметр конструктора "кол-во вагонов" при этом можно удалить.
+# К пассажирскому поезду можно прицепить только пассажирские, к грузовому - грузовые.
